9fc7c39c46c43bc04d1229a2621e7c00cecb2f1f
NitFei <NitFei@users.noreply.github.com>
2020-12-09 20:00:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -11,11 +11,12 @@
 		autoplay: false,
 		loop: true;
 	});*/
-	var sound = new Howl({
-	  src: ['audio/test.wav']
+	var testSound = new Howl({
+		src: ['audio/test.wav'],
+		autoplay: false,
+		loop: true
 	});
 
-	sound.play();
 
 	let deinTextTimer = 0;
 	let titelLabelTimer = 0;
